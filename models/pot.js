7bc1c02cfb0bf66fb87340703a3c59cf3494d576
Rodrigo Lagos <rodrigo.lagos.13@sansano.usm.cl>
2017-09-11 04:07:29 -0300

    Update pot model

--- models/pot.js
+++ models/pot.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const PotSchema = Schema({
-  name: String,
-  description: String
+  humidity: Number,
+  moisture: Number,
+  roomTemperature: Number,
+  temperature: Number,
+  owner: {type: Schema.ObjectId, ref: 'User'},
+  watchers: [{type: Schema.ObjectId, ref: 'User'}]
 })
 
 module.exports = mongoose.model('Pot', PotSchema)
