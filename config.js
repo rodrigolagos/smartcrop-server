85ceff2a58269c01b3a93e91dc3b1f497cdd518c
Rodrigo Lagos <rodrigo.lagos.13@sansano.usm.cl>
2017-10-08 01:12:20 -0300

    Change mongodb connection string

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 8080,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/smartcrop',
+  db: process.env.MONGODB || 'mongodb://127.0.0.1:27017/smartcrop',
   SECRET_TOKEN: 'smartcrop-token'
 }
